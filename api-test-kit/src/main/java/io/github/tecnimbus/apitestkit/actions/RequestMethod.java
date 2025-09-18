b575c9026bb83e0a15b51664e98741fc59524a37
Bathiya Ladduwahetty <dbladduwahetty@gmail.com>
2025-09-18 23:52:00 +1200

    add request params

---
+++ api-test-kit/src/main/java/io/github/tecnimbus/apitestkit/actions/RequestMethod.java
@@ -0,0 +1,11 @@
+package io.github.tecnimbus.apitestkit.actions;
+
+public enum RequestMethod {
+    GET,
+    POST,
+    PUT,
+    DELETE,
+    PATCH,
+    HEAD,
+    OPTIONS
+}
--- api-test-kit/src/main/java/io/github/tecnimbus/apitestkit/actions/RestCaller.java
+++ api-test-kit/src/main/java/io/github/tecnimbus/apitestkit/actions/RestCaller.java
@@ -1,6 +1,17 @@
 package io.github.tecnimbus.apitestkit.actions;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class RestCaller {
+    public static RequestMethod requestMethod = RequestMethod.GET;
     public static String baseURI = "http://localhost";
     public static String endpoint = "";
+    public static Map<String, String> headers = new HashMap<>();
+    public static Map<String, String> queryParams = new HashMap<>();
+    public static String requestBody = "";
+
+    public static void send() {
+      // TODO: Implement the logic to send the HTTP request using the specified parameters.
+    }
 }
